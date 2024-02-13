fbe102faf94aa332b0270ec2128adfb3923de6de
aditya <adityakumarsinha0000@gmail.com>
2024-02-13 16:46:11 +0530

    2ns Commit

--- app/src/main/java/com/example/imekeyboard/MainActivity.kt
+++ app/src/main/java/com/example/imekeyboard/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.imekeyboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.keyboard)
-
+        Log.d("MainActivity", "onCreate: ")
     }
 }
